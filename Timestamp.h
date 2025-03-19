f0bbbce0bffd7622b91412ae76c3eb3a89cecae4
Joyfine_ubuntu <2093730260@qq.com>
2025-03-19 16:15:15 +0800

    完成Timestamp类 补充Logger.cc 的时间部分

---
+++ Timestamp.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class Timestamp {
+public:
+    Timestamp(); // 默认构造
+    explicit Timestamp(int64_t microSecondsSinceEpoch); // 非默认构造 explicit防止隐式转换
+    static Timestamp now(); // 类中的静态成员函数 不依赖任何实例对象
+    std::string toString() const;
+private:
+    int64_t microSecondsSinceEpoch_;
+};
